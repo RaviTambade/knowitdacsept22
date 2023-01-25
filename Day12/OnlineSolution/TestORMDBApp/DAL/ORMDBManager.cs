eeae43751ccb96ea61318bbd49b1f4c8f92d934d
unknown <ravi.tambade@transflower.in>
2023-01-25 14:37:47 +0530

    Entity Frameowork CRUD Operations

--- Day12/OnlineSolution/TestORMDBApp/DAL/ORMDBManager.cs
+++ Day12/OnlineSolution/TestORMDBApp/DAL/ORMDBManager.cs
@@ -1,6 +1,4 @@
-
 using BOL;
-
 namespace DAL;
 public  static class ORMDBManager{
 
@@ -13,4 +11,53 @@ public static List<Employee> GetAll(){
         return employees.ToList<Employee>();
     }
 }
+
+
+public static  Employee  GetById(int id){
+    Employee foundEmployee =null;
+    //Disposing
+    using(var context=new CollectionContext())
+    {
+        var emp = context.Employees.FirstOrDefault(s => s.Id ==id);
+        context.SaveChangesAsync();
+        foundEmployee=emp;    
+    }
+    return foundEmployee;
+}
+
+public static bool Insert(Employee emp){
+    bool status=false;
+    using(var context=new CollectionContext())
+    {
+            context.Add(emp);
+            context.SaveChangesAsync();
+            status=true;
+    }
+    return status;
+}
+
+public static bool Update(Employee emp){
+    bool status=false;
+    using(var context=new CollectionContext())
+    {  
+      var studentToUpdate = context.Employees.FirstOrDefault(s => s.Id ==emp.Id);
+  
+         context.Add(emp);
+            context.SaveChangesAsync();
+            status=true;
+    }
+    return status;
+}
+public static bool Remove(int id){
+  bool status=false;
+  using(var context=new CollectionContext())
+  {
+    var studentToDelete = context.Employees.Find(id);
+    context.Employees.Remove(studentToDelete);
+    context.SaveChangesAsync();
+    status=true;
+    }
+    return status;
+}
+
 }
